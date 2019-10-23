4b6cc52a1f98f3111757e45874ed0b654548213e
Rorisang MOELETSI <rmoelets@c4r18s4.wethinkcode.co.za>
2019-10-23 07:24:52 +0200

    .

---
+++ Cam/cam.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="cam.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+<div class="booth">
+        <video id="video" width="400" height="300"></video>
+        <a href="#" id="capture" class="booth-capture-button">
+            Take photo
+        </a> 
+        <canvas id="canvas" width="400" height="300">
+        </canvas> 
+    <script src="photo.js"></script>
+</body>
+</html>
