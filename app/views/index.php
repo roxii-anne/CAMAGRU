73f4de5f275d31c307f97c771abf1cc1dc91933e
Rorisang MOELETSI <rmoelets@c4r18s4.wethinkcode.co.za>
2019-11-05 18:33:20 +0200

    .

---
+++ app/views/index.php
@@ -0,0 +1,7 @@
+<?php $this->setTitle('Home'); ?>
+<?php $this->start('head'); ?>
+<?php $this->end(); ?>
+
+<?php $this->strat('body'); ?>
+<h1> Welcome to Camagru </h1>
+<?php $this->end(); ?>
---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DEBUG', true);
+
+//default controller if tjere is not one defined in the url
+    define('DEFAULT_CONTROLLER', 'Home');
+//if no layout is set in the controller use this layout
+    define('DEFAULT_LAYOUT', 'default'); 
+
+    define('PROOT', '/camagru/'); //set this to '/' for a live server 
+
+    
+//this will be sued if no site title is set
+    define('SITE_TITLE', 'camagru');
